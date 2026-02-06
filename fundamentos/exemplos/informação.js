fb1303d89d06123c09173ac6b569e3317df57ff0
Murilo cardoso <murilo.c.cardoso@aluno.senai.br>
2026-02-06 09:23:04 -0300

    arquivo informação

---
+++ fundamentos/exemplos/informação.js
@@ -0,0 +1,12 @@
+const curso = "Técnico em Desenvolvimento de Sistemas"
+const ano = 2025
+const notaMinimaparaseraprovado = 50
+const notaMinimapraserreprovado = 49.9
+const cursoTecnico = true
+
+console.log("minhas informações acâdemicas")
+console.log("curso: ", curso)
+console.log(`Ano: ${ano}`)
+console.log(`nota para aprovação ${notaMinimaparaseraprovado}`)
+console.log(`nota minima para ser reprovado  ${notaMinimapraserreprovado}`)
+console.log(`E curso técnico: ${cursoTecnico}`)
